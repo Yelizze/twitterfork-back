b02a08af31ed79a6c74f5e12fccc447d07bfdc0f
Félix msy <felixmansy@hotmail.com>
2023-03-23 14:42:45 +0100

    cors

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+//*
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+
+var app = express();
 const cors = require("cors");
 app.use(cors());
-var app = express();
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
