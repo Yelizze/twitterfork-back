f3023227acaa673844374355d53aab85d6fa9b98
Félix msy <felixmansy@hotmail.com>
2023-06-19 16:29:25 +0200

    d

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString =
+  "mongodb+srv://bulleouvrard:83ZvEdihFcWyKHZH@cluster0.ebmpybm.mongodb.net/hackatweet";
 
-mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch(error => console.error(error));
+mongoose
+  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.error(error));
